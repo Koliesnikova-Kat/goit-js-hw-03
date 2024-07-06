a53500954d1b9b9c55cb5fd4dc3f755dccad2414
Kateryna <koliesnikova.kateryna@gmail.com>
2024-07-07 00:07:31 +0300

    v2

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,23 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
+// function slugify(title) {
 
-  const totalPrice = quantity * pricePerDroid;
+//   const titleLower = title.toLowerCase();
+//   return titleLower.join("-");
+// }
 
-  if (totalPrice > customerCredits) {
-  return "Insufficient funds!";
-  }
-  
-  else {
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
-}
+// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+
+
+// // перебирати всі значення масиву tolowercase
+
+
+
+
+
+
+
+
--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,23 @@
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-    return (message);
-  }
+// function makeArray(firstArray, secondArray, maxLength) {
+//   const result = firstArray.concat(secondArray);
+//   if (result > maxLength) {
+//     return result.slice(0, `${maxLength}` - 1);
+//   } else {
+//     return result;
+//   }
+// }
 
-  else {
-    return message.slice(0, maxLength) + "..."
-  }
-}
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+
+
+
+
+
+
